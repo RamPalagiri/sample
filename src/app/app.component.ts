5a69687da6c23c5dba4f168bbc5012bb56b1c36f
RPalagiri <RPalagiri@INHYDLRPALAGIRI>
2021-03-09 14:51:47 +0530

    ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: <p>AngularDemo</p>,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
