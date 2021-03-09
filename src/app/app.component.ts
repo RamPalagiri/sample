03ae2d3138ec9be566fe7977a224c1146957f7e4
RPalagiri <RPalagiri@INHYDLRPALAGIRI>
2021-03-09 14:58:36 +0530

    ssecond

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // template: <p>AngularDemo</p>,
+  // template: <p>AngularDemo</p> aehfasfkdaksdfabf,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
